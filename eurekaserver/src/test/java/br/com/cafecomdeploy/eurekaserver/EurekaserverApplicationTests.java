fe745b00862aa2794f4d37e60be2460240efd81e
cafecomdeploy <lorena.cunha@hotmail.com>
2022-11-19 18:59:23 -0300

    Criacao e configuracao inicial Eureka Server

---
+++ eurekaserver/src/test/java/br/com/cafecomdeploy/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cafecomdeploy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
